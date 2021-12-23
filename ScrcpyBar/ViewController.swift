91717794cc7b807bd93dcd556da6905283bf0750
fatkhur <fatkhuranonym@gmail.com>
2021-12-23 23:32:40 +0700

    Initial Commit

---
+++ ScrcpyBar/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  ScrcpyBar
+//
+//  Created by Patur on 23/12/21.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
